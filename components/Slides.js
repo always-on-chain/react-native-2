8975532845337c57c3cd1a6f718bd8af46123b6a
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-11-18 16:41:52 -0800

    added some styles to Slide component

--- components/Slides.js
+++ components/Slides.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView} from 'react-native';
 
 class Slides extends Component {
+  renderSlides() {
+    console.log('data', this.props.data)
+    return this.props.data.map((slide) => {
+      return (
+        <View style={styles.slideStyle} key={slide.text}>
+          <Text style={styles.textStyle}>{slide.text}</Text>
+        </View>
+      );
+    })
+  }
+
   render() {
     return (
-      <View>
-      </View>
+      <ScrollView
+        horizontal={true}
+        // style={{ flex: 1 }}
+      >
+        {this.renderSlides()}
+      </ScrollView>
     );
+  }
+}
+
+const styles = {
+  slideStyle: {
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  textStyle: {
+    fontSize: 30
   }
 }
 
