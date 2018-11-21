fe1248fb634f2032f7c2d5eadba08a47a91403eb
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-11-21 12:05:26 -0800

    created helper function to actually execute the facebook login process

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,17 +1,37 @@
 import { AsyncStore } from 'react-native';
+import { Facebook } from 'expo';
+
 import {
-  FACEBOOK_LOGIN_SUCCESS
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL,
 } from './types';
+import { AsyncStorage } from '../../../Library/Caches/typescript/2.6/node_modules/@types/react-native';
 
 // How to use AsyncStorage:
 // AsyncStorage.setItem('fb_token', token);
 // AysncStorage.getItem('fb_token')
 
-export const facebookLogin = () => async dispatch => {
+export const facebookLogin = () => async (dispatch) => {
   let token = await AsyncStorage.getItem('fb_token');
+
   if (token) {
     // Dispatch an action saying FB loign is done
+    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
   } else {
     // Start up FB Login process
+    doFacebookLogin(dispatch);
   }
+};
+
+const doFacebookLogin = async (dispatch) => {
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync('369346263609611', {
+    permissions: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    return dispatch({ type: FACEBOOK_LOGIN_FAIL })
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
 };
--- actions/types.js
+++ actions/types.js
@@ -1,1 +1,2 @@
 export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_succes';
+export const FACEBOOK_LOGIN_FAIL = 'facebook_login_fail';
